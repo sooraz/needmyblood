1de0c062b74cf6e15aea355ab87f54302f666e0e
sooraz <boddetisooraz@gmail.com>
2019-02-11 21:59:48 +0530

    login basic

--- NeedForBloodV1/app/src/main/java/com/example/needforbloodv1/MainActivity.java
+++ NeedForBloodV1/app/src/main/java/com/example/needforbloodv1/MainActivity.java
@@ -8,81 +8,104 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     EditText mUserName,mPassword;
+    TextView mErrorText;
     ProgressDialog pDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mUserName=findViewById(R.id.editText);
+        mPassword=findViewById(R.id.editText2);
+        mErrorText=findViewById(R.id.errorText);
     }
 
     public void start(View view) {
         switch(view.getId()){
             case R.id.login:
-                mUserName=findViewById(R.id.editText);
-                mPassword=findViewById(R.id.editText2);
                 login(mUserName.getText().toString(),mPassword.getText().toString());
                 break;
             case R.id.register:
                 break;
         }
     }
-    private void login(final String name, final String password){
+    private void login(final String name, final String password) {
         String url = "http://sooraz.000webhostapp.com/login.php";
-        pDialog = new ProgressDialog(this);
-        pDialog.setMessage("Loading...");
-        pDialog.show();
-        StringRequest strReq = new StringRequest(Request.Method.GET,
-                url,
-                new Response.Listener<String>() {
+        if (name == null || name.equals("")) {
+            mErrorText.setVisibility(View.VISIBLE);
+            mErrorText.setText("enter name");
 
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            Log.d("sooraz", response);
-                            
-                        } catch (Exception e) {
-                            e.printStackTrace();
+        } else if (password == null || password.equals("")) {
+            mErrorText.setVisibility(View.VISIBLE);
+            mErrorText.setText("enter password");
+        } else {
+            mErrorText.setVisibility(View.GONE);
+            pDialog = new ProgressDialog(this);
+            pDialog.setMessage("Loading...");
+            pDialog.show();
+            StringRequest strReq = new StringRequest(Request.Method.POST,
+                    url,
+                    new Response.Listener<String>() {
+
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                Log.d("sooraz", response);
+                                JSONObject temp = new JSONObject(response);
+                                switch (temp.getInt("success")) {
+                                    case 0:
+                                        mErrorText.setVisibility(View.VISIBLE);
+                                        mErrorText.setText(temp.getString("message"));
+                                        break;
+                                    case 1:
+                                        Log.d("sooraz", "Logeed in success");
+
+                                }
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            pDialog.hide();
                         }
-                        pDialog.hide();
-                    }
-                }, new Response.ErrorListener() {
+                    }, new Response.ErrorListener() {
 
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d("sooraz", "Error: " + error.getMessage());
-                pDialog.hide();
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("sooraz", "Error: " + error.getMessage());
+                    pDialog.hide();
+                }
+
             }
+            ) {
 
-        }
-        )
-        {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("name", name);
+                    params.put("password", password);
+                    //params.put("password", "password123");
+                    Log.d("sooraz", " in map " + params);
 
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("name", name);
-                params.put("password", password);
-                //params.put("password", "password123");
-                Log.d("sooraz", " in map "+params );
+                    return params;
+                }
 
-                return params;
-            }
-
-        };
+            };
 
 // Adding request to request queue
-        AppContoller.getInstance().addToRequestQueue(strReq);
+            AppContoller.getInstance().addToRequestQueue(strReq);
+        }
     }
 }
