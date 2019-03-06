b49c8519a5f85c8064d9f230a3cb87b6970a32cd
sooraz <sooraz@users.noreply.github.com>
2019-03-06 10:27:04 +0530

    Update register.php
    adding check for db to avoid duplicate inserts

--- For_php/register.php
+++ For_php/register.php
@@ -8,7 +8,6 @@ $conn = new mysqli($servername, $username, $password, "id8154736_test");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 $response = array(); 
 if (isset($_POST['name']) && isset($_POST['password'])&& isset($_POST['mail'])&& isset($_POST['gender'])&& isset($_POST['bgroup'])&& isset($_POST['location'])) {
  
@@ -18,21 +17,33 @@ if (isset($_POST['name']) && isset($_POST['password'])&& isset($_POST['mail'])&&
     $gender = $_POST['gender'];
     $bgroup = $_POST['bgroup'];
     $loc = $_POST['location'];
-    $sql = "INSERT INTO my_blood (Name,Password,Location_Permanent,Blood_Group,Email,Gender,Location_temp) VALUES ('".$name."','".$pass."','".$loc."','".$bgroup."','".$mail."','".$gender."','".NULL."')";
-    if ($conn->query($sql) === TRUE) {
-        $response["success"] = 1;
-        $response["message"] = "successfully registered";
- 
-        // echoing JSON response
+    $sql = "SELECT * FROM my_blood WHERE Name='".$name."' && Password='".$pass."'";
+    $result=mysqli_query($conn,$sql);
+    $num= mysqli_num_rows($result);
+    if($num>0){
+    	$response["success"] = 0;
+    	$response["message"] = "already registered";
+
+    	// echoing JSON response
         echo json_encode($response);
-    } else {
+    }    
+    else{
+    $sql = "INSERT INTO my_blood (Name,Password,Location_Permanent,Blood_Group,Email,Gender,Location_temp) VALUES ('".$name."','".$pass."','".$loc."','".$bgroup."','".$mail."','".$gender."','".NULL."')";
+    	if ($conn->query($sql) === TRUE) {
+        	$response["success"] = 1;
+        	$response["message"] = "successfully registered";
+ 
+        	// echoing JSON response
+        	echo json_encode($response);
+    	} else {
         // failed to insert row
         $response["success"] = 0;
         $response["message"] = "invalid registration";
  
         // echoing JSON response
         echo json_encode($response);
-    }
+    	}
+	}
 } else {
     // required field is missing
     $response["success"] = 0;
